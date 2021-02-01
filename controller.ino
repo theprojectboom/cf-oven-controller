b205c55aef88661a405e2db7347e29e269814661
Minos Park <minos@theprojectboom.org>
2021-02-01 08:49:40 -0800

    initial commit with pid code

---
+++ controller.ino
@@ -0,0 +1,37 @@
+#include "pid.h"
+
+// Pin numbers are arbitrary - change it before flashing
+#define thermocouple_pin A6
+#define triac_in_pin A7
+#define triac_out_pin A8
+
+
+// Create pid object with params
+double dt = 0.1;          // loop interval time
+double max_out = 100;     // maximum allowable output from pid
+double min_out = -100;    // minimum allowable output from pid
+double Kp = 0.1;          // proportional gain
+double Kd = 0.01;         // derivative gain
+double Ki = 0.5;          // integral gain
+PID pid = PID(dt, max_out, min_out, Kp, Kd, Ki);
+
+double test_current_val = 20;
+double test_setpoint = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Calculate this interval's control output
+  double control_output = pid.calculate(test_setpoint, test_current_val);
+
+  char strBuf[60];
+  sprintf(strBuf, "current_val:% 7.3f control_output:% 7.3f\n", test_current_val, control_output);
+  Serial.print(strBuf);
+
+  // For test, assume the control_output is fully realized
+  test_current_val += control_output;
+}
