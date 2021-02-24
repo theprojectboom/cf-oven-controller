d25f4dcfc7f02135f89684c785bda97a1dde69e8
Minos Park <minos@theprojectboom.org>
2021-02-24 13:27:21 -0800

    add thermocouple readings

--- controller.ino
+++ controller.ino
@@ -1,27 +1,37 @@
 #include "pid.h"
+#include "thermocouple.h"
 #include "LiquidCrystal_I2C.h"
 
 
 // Pin numbers are arbitrary - change it before flashing
-#define thermocouple_pin A6
-#define triac_in_pin A7
-#define triac_out_pin A8
 #define LCD_PRESENT true
+#define LCD_ADDRESS 0x27
+#define TEMP_CALIBRATION true
 
-// Set the LCD address to 0x27
-LiquidCrystal_I2C lcd(0x27,20,4);
+// Setup the LCD Matrix
+LiquidCrystal_I2C lcd(LCD_ADDRESS,20,4);
 
-// Create pid object with params
+// Setup the thermocouples, use farenheight
+// For now, use one thermocouple for ease of setup and testing
+Thermocouple t1(0, false, 0.5);
+Thermocouple t2(0, false, 0.5);
+Thermocouple t3(0, false, 0.5);
+Thermocouple t4(0, false, 0.5);
+Thermocouple t5(0, false, 0.5);
+
+// PID object params
 double dt = 0.1;          // loop interval time
-double max_out = 100;     // maximum allowable output from pid
-double min_out = -100;    // minimum allowable output from pid
-double Kp = 0.1;          // proportional gain
+double max_out = 1;     // maximum allowable output from pid
+double min_out = -1;    // minimum allowable output from pid
+double Kp = 0.01;          // proportional gain
 double Kd = 0.01;         // derivative gain
 double Ki = 0.5;          // integral gain
+
+// Create pid object with params
 PID pid = PID(dt, max_out, min_out, Kp, Kd, Ki);
 
-double test_current_val = 20;
-double test_setpoint = 0;
+// Variables for test/debug
+double test_setpoint = 60;
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,32 +48,52 @@ void setup() {
     Serial.print("LCD Initialized.\n You should see \"Hello, welcome!\" for 5 seconds.");
     delay(5000);
   }
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (TEMP_CALIBRATION) {
+    // Log the temperature to serial output
+    // We'll save this to an output file on a companion computer
+    Serial.print("T1, ");
+    Serial.print(t1.read());
+    Serial.println("");
+
+    Serial.print("T2, ");
+    Serial.print(t2.read());
+    Serial.println("");
+
+    Serial.print("T3, ");
+    Serial.print(t3.read());
+    Serial.println("");
+
+    Serial.print("T4, ");
+    Serial.print(t4.read());
+    Serial.println("");
+
+    Serial.print("T5, ");
+    Serial.print(t5.read());
+    Serial.println("");
+  }
+
+  // For now, we'll average all the temp readings in F
+  double avg_temp = (t1.read() + t2.read() + t3.read() + t4.read() + t5.read()) / 5;
 
   // Calculate this interval's control output
-  double control_output = pid.calculate(test_setpoint, test_current_val);
-
-  Serial.print("current_val: ");
-  Serial.print(test_current_val);
-  Serial.print(", control_output: ");
-  Serial.print(control_output);
-  Serial.println("");
-  delay(300);
-
-  // For test, assume the control_output is fully realized
-  test_current_val += control_output;
+  double control_output = pid.calculate(test_setpoint, avg_temp);
 
   if (LCD_PRESENT) {
     // Refresh the LCD screen
     lcd.clear(); // Clear the screen
     lcd.setCursor(3,0);
-    lcd.print("Current value: ");
-    lcd.print(test_current_val);
+    lcd.print("Curr Temp (F): ");
+    lcd.print(avg_temp);
     lcd.setCursor(3,1);
-    lcd.print("Control output: ");
+    lcd.print("Ctrl Ouput: ");
     lcd.print(control_output);
   }
+
+  // Delay the loop for human readable debugging
+  delay(300);
 }
