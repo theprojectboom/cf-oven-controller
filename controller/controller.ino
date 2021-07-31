fe30711dc2c68c497278f219b638b1be804a7100
Minos Park <minos@theprojectboom.org>
2021-07-31 16:56:58 -0700

    adjust lcd output

--- controller/controller.ino
+++ controller/controller.ino
@@ -129,14 +129,15 @@ void loop() {
     lcd.print(USE_CELCIUS ? "C.": "F.");
 
     lcd.setCursor(0,1);
-    lcd.print("Time left (min): ");
-    unsigned long time_left_min = (CURE_NOMINAL_HOURS_MINIMUM * 1000 * 3600 - (millis() - setpoint_reached_timestamp)) / (60.0 * 1000);
-    lcd.print(time_left_min);
-
-    lcd.setCursor(0,2);
     lcd.print("Time on (min): ");
     unsigned long time_on_min = millis() / (60.0 * 1000);
     lcd.print(time_on_min);
+
+    lcd.setCursor(0,2);
+    lcd.print("Time left (min): ");
+    long time_elapsed_since_setpoint_reached = millis() - setpoint_reached_timestamp;
+    long time_left_min = (CURE_NOMINAL_HOURS_MINIMUM * 1000 * 3600 - time_elapsed_since_setpoint_reached ) / (60.0 * 1000);
+    lcd.print(update_setpoint_timestamp ? 480 : time_left_min);
 
     last_lcd_update_timestamp = current_meas_timestamp;
   }
